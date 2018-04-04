2cdf49330d02490f19f21ae8966c3c0247c3492d
J H <jahalu88@hotmail.com>
2018-04-04 18:32:13 -0400

    finished styling and added readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # TriviaGame
+This game was created by Jaime Halupka as a requirement for homework during University of Central Florida's Coding
+Bootcamp. 
+
+![Closing Screen](/assets/images/screenshot.png "Closing Screen")
+
+**To Play**
+Click 'start' to begin the trivia questions. You will have thirty seconds to answer each multiple choice question. Check the correct button and click 'Next'. There are 10 questions total. When you have completed the quiz, the number of correct answers out of ten will be displayed. Good Luck!
+
+**Thank you**
+The two photographs for this project were taken by Jaime Halupka. The opening image is a hummingbird (taken in Costa Rica) and the closing image is a hermit thrush (taken in New Jersey).
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,33 @@
+
+// Ready after document loads
 $(document).ready(function () {
 
+// Function to create quiz set
     function Quiz(question, choices, correct) {
         this.question = question;
         this.choices = choices;
         this.correct = correct;
     }
-
+// Array to hold questions
     var all = [
         new Quiz("A typical life span for a scarlet macaw is:", ["10 years", "20 years", "40 years", "50+ years"], 3),
-        new Quiz("A bald eagle has a wingspan of 7 ft and weighs:", ["10lbs", "15lbs", "20lbs"], 0),
+        new Quiz("A bald eagle has a wingspan of seven feet and weighs:", ["10 lbs", "15 lbs", "20 lbs"], 0),
         new Quiz("The penguin is the only bird that has fur.", ["True", "False"], 1),
-        new Quiz("A peregrine falcon can reach flight speeds of:", ["50mph", "25mph", "100mph", "200mph"], 3),
-        new Quiz("Poisonous birds exist", ["True", "False'"], 0),
-        new Quiz("Which one is an example of a bird that cannot fly?", ["turkey vulture", "pileated woodpecker", "Rhea"], 2),
+        new Quiz("A peregrine falcon can reach flight speeds of:", ["50 mph", "25 mph", "100 mph", "200 mph"], 3),
+        new Quiz("Poisonous birds exist", ["True", "False"], 0),
+        new Quiz("Which one is an example of a bird that cannot fly?", ["turkey vulture", "pileated woodpecker", "rhea"], 2),
         new Quiz("Feathers do not regrow", ["True", "False"], 1),
         new Quiz("The color of a chicken's egg can be predicted by the color of its:", ["feet", "earlobe", "feathers"], 1),
         new Quiz("The only bird that can fly backwards is the:", ["chicken", "falcon", "hummingbird"], 2),
         new Quiz("An owl can rotate its head", ["100 degrees", "270 degrees", "360 degrees"], 2)
 
     ];
-
+// Globals
     var currentQ = 0;
     var correctA = 0;
     var timeLeft = 30;
 
-
+// Function to render each question and choices
     function renderQuiz() {
         $('#question').html(parseInt(currentQ) + 1 + ". " + all[currentQ].question);
         var opts = all[currentQ].choices;
@@ -37,13 +40,14 @@ $(document).ready(function () {
         timer();
     }
 
+// Function to check if answer is correct
     function check() {
         if ($("input[name=option]:checked").val() == all[currentQ].correct) {
             correctA++;
         }
     };
-// 
-    // var timerId = setTimeout(timer, 1000);
+
+// Function to control 30 second timer
     function timer() {
         console.log("timer");
         var timerId = setInterval(timer, 1000);
@@ -80,47 +84,42 @@ $(document).ready(function () {
         }
     };
 
-
+// variables to make assinment easier
     var $jumbotron = $(".jumbotron");
     var $next = $("#next");
     var $result = $("#result");
     var $start = $("#start");
-    // var timerId = setInterval(timer, 1000);
-
+   
+// Starting state with jumbotron hidden
     $jumbotron.hide();
+    $("#birdend").hide();
     $start.click(function () {
         $jumbotron.show();
         $(this).hide();   
         $("#birdstart").hide();   
         renderQuiz();
         
-
     });
 
     
-
+// Next button controls
     $next.click(function () {
-        // event.preventDefault();
         check();
         currentQ++;
         if (currentQ < all.length) {
             renderQuiz();
-            // clearTimeout(timerId);
-            // timeLeft = 30;}
+            
         }
-
-
-
-            if (currentQ == all.length - 1) {
-                $next.html("Submit");
-                $next.click(function () {
-                    $jumbotron.hide();
-                    $result.html("You correctly answered " + correctA + " out of " + currentQ + " questions").hide();
-                    $result.fadeIn(1500);
+        if (currentQ == all.length - 1) {
+            // Changes next button to "submit"
+            $next.html("Submit");
+            $next.click(function () {
+            $jumbotron.hide();
+            $result.html("You correctly answered " + correctA + " out of " + currentQ + " questions").hide();
+            $result.fadeIn(1500);
+            $("#birdend").show();
                 });
-        
-
-        };
+            };
     });
 });
 
